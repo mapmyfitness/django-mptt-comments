6f6da7fe43c1b27df2ba6933e9db0c6700a6641f
Dave McLain <dave.mclain@mapmyfitnessinc.com>
2014-01-20 17:58:31 -0600

    CORE-472 integrate release tools

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@ from setuptools import setup, find_packages
 
 long_description = open('README.rst').read()
 
+import sys
+
+if 'sdist' in sys.argv:
+    import mmf_release_tools
+    version = mmf_release_tools.generate_release_version("0.1", __file__)
+    mmf_release_tools.write_release_version(version)
+else:
+    with open("RELEASE-VERSION", "r") as f:
+        version = f.readlines()[0].strip()
+
+
 setup(
     name='django-mptt-comments',
-    version="0.1",
+    version=version,
     package_dir={'mptt_comments': 'mptt_comments'},
     packages=['mptt_comments', 'mptt_comments.templatetags'],
     description='Django Mptt Comments is a simple way to display threaded comments instead of the django contrib comments.',
